5f380d96ec6bd6a5327a50cc1433e4e20d6f8825
Kudzai Nyika <kudzainyika@Kudzais-MacBook-Air.local>
2023-09-29 08:50:55 +0200

    finished newline issue

--- increase_brightness.asm
+++ increase_brightness.asm
@@ -26,47 +26,58 @@ main:
     move $s1, $v0          # save output file descriptor
     
     # Read from input, modify, write to output
-    li $9, 10       #constant value to add
-
+    li $t9, 10       #constant value to add
+    li  $t7, 48 #stores ascii value of zero
+    
 read_loop:
     li $v0, 14      #read from file
     la $a1, buffer  #buffer to read into
     li $a2, 12292    #number of bytes to read 
-    syscall         #make system call
+    syscall         
     
     beqz $v0, end_of_file #exit loop if read returns zero
     
     # Process each line in the buffer
     la $t0, buffer      #load address of the buffer
-    li $t3, 4           #used to ensure first 4 lines are ignored
+    li $t5, 0           #stores current pixel value
+    
+    #li $t3, 4           #used to ensure first 4 lines are ignored
+    #li $t4, 0           # used to indicate once we've passed the fourth line
     
 process_line_loop:
-    
-    lb $t1, 0($t0)  # load a byte from buffer
-    beq $t1, 10, end_of_line   # newline character
-    beqz $t1, end_of_line      # null terminator (end of buffer)
-    
-    #Add 10 to value 
-    addi $t1, $t1, 10
-    
-    sb $t1, 0($t0)             #Store modified character in buffer
-    addi $t0, $t0, 1           #move to next char in buffer
-    
-    li $t2, 255                # maximum allowed value
-    
-    bgt $t1, $t2, clamp        # branch if value is greater than 255
-    j process_line_loop
+
+    process_num: 
+        lb $t1, 0($t0) #load a byte from buffer
+        beq $t1, 10, add_ten       #newline character - end of line
+        beqz $t1, end_of_file      # null terminator (end of buffer)
+        
+        sub $t1, $t1, $t7 #subtracting ascii value of zero turns it into an integer
+        mul $t5,$t5,$t9             # multiply number by 10
+        add $t5, $t5, $t1           # add character to total
+        j process_num
+        
+    add_ten:     
+        #Add 10 to value 
+        addi $t5, $t5, 10
+        
+        li $t2, 255                # maximum allowed value
+        bgt $t5, $t2, clamp        # branch if value is greater than 255
+        sb $t5, 0($t0)             # Store modified character in buffer
+        addi $t0, $t0, 1           # move to next char in buffer
+        j process_line_loop
 
 clamp:
-    li $t1, 255                     # Set $t1 to 255 if it exceeds 255
-    sb $t1, 0($t0)                  # Store the modified character back to the buffer
+    li $t5, 255                     # Set $t1 to 255 if it exceeds 255
+    
+    sb $t5, 0($t0)                  # Store the modified character back to the buffer
+    addi $t0, $t0, 1                # move to next char in buffer
     j process_line_loop         # Continue processing
     
-
-end_of_file:
-    j process_line_loop         # Perform cleanup for the end of the line
     
-close_input_file:
+#ending:
+# j process_line_loop         # Perform cleanup for the end of the line
+    
+end_of_file:
     li $v0, 16             # syscall: close file
     move $a0, $s0          # input file descriptor
     syscall               
